4d3e123d7bb19caf9d9f9d43711185b36fdf267a
Armando López Torres <47039613+mandoveloper@users.noreply.github.com>
2025-04-15 16:10:52 -0500

    Create plugin-test.php

---
+++ plugin-test.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Atix Payment Gateway for Woocommerce
+ *
+ * This is a simple payment gateway for Atix Payment Services.
+ *
+ * @link              https://dashboard.atix.com.pe/
+ * @since             1.0.0
+ * @package           woocommerce_atix
+ *
+ * @wordpress-plugin
+ * Plugin Name:       plugin-test
+ * Description:       Example
+ * Version:           1.0.0
+ * Author:            Mando
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       woocommerce-atix
+ * Domain Path:       /languages
+ */
